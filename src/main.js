8a00069f13fa6016214e39d1f5758c51175691c7
“karificity” <1403087147@qq.com>
2020-07-15 01:28:40 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
 // axios拦截器
@@ -18,6 +22,19 @@ axios.interceptors.request.use( config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function(originVal){
+	const dt = new Date(originVal)
+	const y = dt.getFullYear()
+	const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+	const d = (dt.getDay() + '').padStart(2, '0')
+	
+	const hh = (dt.getHours() + '').padStart(2, '0')
+	const mm = (dt.getMinutes() + '').padStart(2, '0')
+	const ss = (dt.getSeconds() + '').padStart(2, '0')
+	
+	return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 
 
