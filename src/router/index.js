a09751bf79652bccda0d310a1018e5ffe42f74ef
“karificity” <1403087147@qq.com>
2020-07-16 12:11:47 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import Params from '../components/goods/params.vue'
 import List from '../components/goods/list.vue'
 import Add from '../components/goods/add.vue'
 import Order from '../components/order/order.vue'
+import Report from '../components/report/report.vue'
+
 
 Vue.use(VueRouter)
 
@@ -30,7 +32,8 @@ const routes = [
 		  { path: '/params', component: Params },
 		  { path: '/goods', component: List },
 		  { path: '/goods/add', component: Add },
-		  { path: '/orders', component: Order }
+		  { path: '/orders', component: Order },
+		  { path: '/reports', component: Report }
 	  ]}
 ]
 
